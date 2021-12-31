5e33cef7c835c307d687d1969903e49b8f848efb
cpark <mansoo1500@gmail.com>
2022-01-01 01:30:02 +0900

    Updates

---
+++ precache-manifest.02f56ce52773f9ecdbd29cb4a216d6e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8491c08ec1f51d9c5a1f39cd1cbeac8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e9939278214156d9cb38",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "d9dc36990dc7167c678d",
+    "url": "./static/js/2.0a329436.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.0a329436.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9939278214156d9cb38",
+    "url": "./static/js/main.df249aea.chunk.js"
+  },
+  {
+    "revision": "6dd6c7e9c08cea8c474a",
+    "url": "./static/js/runtime-main.d7bb3688.js"
+  }
+]);
